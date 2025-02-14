2a427aa56998315a2782195224ed0d18462e0a9f
ductohno <152991010+ductohno@users.noreply.github.com>
2025-02-14 18:39:04 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ductohno.vercel.app/",
+  site: "https://ductohno.github.io/",
   base: "/",
   trailingSlash: "always",
   integrations: [
